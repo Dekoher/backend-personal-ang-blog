d67d6aea4b9ad75265ba6dbbb78e9fbfbd784090
Diegoher <diegoher92@gmail.com>
2020-08-07 21:17:40 -0500

    adding articles route to get each articles

--- server.js
+++ server.js
@@ -5,3 +5,9 @@ const app = express();
 app.listen(8000, () => {
   console.log('Server listening on port 8000');
 });
+
+app.get('/', function(req, res) {
+  res.send('Hello from node.js!');
+});
+
+require('./articles')(app);
