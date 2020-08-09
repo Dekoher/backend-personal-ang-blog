82de924f2e47ca76b3e1cdd9295bc609be912578
Diegoher <diegoher92@gmail.com>
2020-08-08 22:17:33 -0500

    adding cors package and using it into main server

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+
+var corsOptions = {
+  origin: ['http://localhost:4200', 'http://localhost:4201']
+};
+
+app.use(cors(corsOptions));
 
 app.listen(8000, () => {
   console.log('Server listening on port 8000');
